68f56be97216af9fb25dfd2b3c067b725125aa5c
ljstaib <45086926+ljstaib@users.noreply.github.com>
2021-03-24 21:29:08 -0400

    Added map and hello world +1 demo button

---
+++ Styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffed',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    height: 300,
+    width: 300
+  },
+  title: {
+    color: "black",
+    fontSize: 72,
+    textAlign: "center",
+  },
+  text: {
+    color: "black",
+    fontSize: 36,
+    textAlign: "center",
+  },
+  button_text: {
+    color: "white",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: '#707070',
+    borderRadius: 5,
+    padding: 1,
+    marginTop: 10,
+    marginBottom: 10,
+    width: "40%",
+  },
+});
